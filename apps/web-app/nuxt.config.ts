c1c4c8052635e85faf3c8c6ce582f970cea83a90
Amereth <alek.melnyk@gmail.com>
2024-10-09 23:41:59 +0300

    primevue setup

--- apps/web-app/nuxt.config.ts
+++ apps/web-app/nuxt.config.ts
@@ -1,5 +1,16 @@
+import Aura from '@primevue/themes/aura'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+	primevue: {
+		options: {
+			ripple: false,
+			theme: {
+				preset: Aura,
+			},
+		},
+	},
 })
