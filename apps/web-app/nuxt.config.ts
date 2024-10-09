02d19e7fbdb47b6d1237a13b4c6d66f3cce64086
Amereth <alek.melnyk@gmail.com>
2024-10-10 00:33:09 +0300

    added prime icons

--- apps/web-app/nuxt.config.ts
+++ apps/web-app/nuxt.config.ts
@@ -2,19 +2,16 @@ import Aura from '@primevue/themes/aura'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				compatibilityDate: '2024-04-03',
-				devtools: { enabled: true },
-				modules: [
-				 '@primevue/nuxt-module',
-				 '@nuxtjs/tailwindcss',
-				 '@nuxt/icon',
-				],
-				primevue: {
-								options: {
-												ripple: false,
-												theme: {
-																preset: Aura,
-												},
-								},
-				},
+	compatibilityDate: '2024-04-03',
+	css: ['./main.css'],
+	devtools: { enabled: true },
+	modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss', '@nuxt/icon'],
+	primevue: {
+		options: {
+			ripple: false,
+			theme: {
+				preset: Aura,
+			},
+		},
+	},
 })
