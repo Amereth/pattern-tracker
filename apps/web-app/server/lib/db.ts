c0d3295a82d51af8108ffba8e650fabc1fc70eee
Amereth <alek.melnyk@gmail.com>
2024-10-09 23:12:57 +0300

    drizzle setup

---
+++ apps/web-app/server/lib/db.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@libsql/client'
+import { drizzle } from 'drizzle-orm/better-sqlite3'
+import * as schema from '../schemas'
+
+const client = createClient({
+	url: process.env.DATABASE_URL!,
+	authToken: process.env.DATABASE_AUTH_TOKEN!,
+})
+
+export const db = drizzle(client, { schema })
