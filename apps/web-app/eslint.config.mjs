2a22837ec977f21d5a3a5fca9ce0317abf683ceb
Amereth <alek.melnyk@gmail.com>
2024-10-12 22:47:00 +0300

    added drizzle eslint

--- apps/web-app/eslint.config.mjs
+++ apps/web-app/eslint.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import drizzle from 'eslint-plugin-drizzle'
 
-export default withNuxt().overrideRules({
-  'vue/no-multiple-template-root': 'off',
-})
+export default withNuxt()
+  .overrideRules({
+    'vue/no-multiple-template-root': 'off',
+  })
+  .prepend({
+    extends: [drizzle],
+  })
 // Your custom configs here
